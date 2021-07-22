cfc3f0cfd56056138d29613cada32d6b8aeadf8a
George Cujbă <george.cujba15@gmail.com>
2021-07-22 16:32:51 +0300

    6 Add Sounds to Button Clicks

--- game.js
+++ game.js
@@ -6,15 +6,16 @@ userClickedPattern = [];
 // An array that contains the colours of the buttons
 buttonColours = ["red", "blue", "green", "yellow"];
 
-// Function that generates a random number between 0 and 3
-function nextSequence() {
-  randomNumber = Math.floor(Math.random() * 4);
+// Function that runs the game
+function nextSequence() {}
 
+function randomNumberGenerator() {
+  var randomNumber = Math.floor(Math.random() * 4);
   return randomNumber;
 }
 
-// Generate a random numnber
-randomNumber = nextSequence();
+// Generate a random Number
+randomNumber = randomNumberGenerator();
 
 // Function that picks an element from the array from a specific index
 function pickElementFromArray(array, index, count) {
@@ -29,27 +30,29 @@ randomChosenColour = pickElementFromArray(buttonColours, randomNumber, 1);
 // Add the randomChosenColour to the end of gamePattern array
 gamePattern.push(randomChosenColour);
 
-// Select a button with the same id as the randomChosenColour
-var selectedButton = $("#" + randomChosenColour);
-
 function flashAnimation(element) {
   element.fadeOut(100).fadeIn(100);
 }
 
+// Select a button with the same id as the randomChosenColour
+var selectedButton = $("#" + randomChosenColour);
+
 // Add flash animation to the selected element
 flashAnimation(selectedButton);
 
-function determineSound(randomColour) {
-  var audio = new Audio("sounds/" + randomColour + ".mp3");
+function playSound(colour) {
+  var audio = new Audio("sounds/" + colour + ".mp3");
   audio.play();
 }
 
-determineSound(randomChosenColour);
+playSound(randomChosenColour);
 
 // Detect which button is clicked and trigger a function
 $(".btn").on("click", function () {
   // Select the id attribute of the clicked button
   var userChosenColour = $(this).attr("id");
+
+  playSound(userChosenColour);
 
   // add the current button's id to the userClickedPattern array
   userClickedPattern.push(userChosenColour);
