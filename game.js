6fc8e9e72488ae3878007220a98487719d289031
George Cujbă <george.cujba15@gmail.com>
2021-07-22 20:26:37 +0300

    10 Implement Game Over Functionality

--- game.js
+++ game.js
@@ -8,61 +8,77 @@ userClickedPattern = [];
 var gameStarted = false;
 var level = 0;
 
+// When a key is pressed run the function
 $(document).keydown(function () {
+  // if gameStarted === false
   if (!gameStarted) {
+    // Change the text of the element with #level-title id to "Level " + level
     $("#level-title").text("Level " + level);
+
+    // Start the game
     nextSequence();
     gameStarted = true;
   }
 });
 
+// Function that generates a random number
 function randomNumberGenerator() {
   var randomNumber = Math.floor(Math.random() * 4);
 
   return randomNumber;
 }
 
+// Function that selects the id attached to the parameter
 function selectColourID(randomColour) {
   var selectedColour = $("#" + randomColour);
 
   return selectedColour;
 }
 
+// Function that adds a flash animation to the parameter
 function flashAnimation(element) {
   element.fadeOut(100).fadeIn(100);
 }
 
-function determineClickedButton() {
-  var clickedButton = $(".btn").on("click", function () {
-    $(this).attr("id");
-  });
-
-  return clickedButton;
-}
-
 // Detect which button is clicked and trigger a function
 $(".btn").click(function () {
+  // Select the id of the selected element
   var userChosenColour = $(this).attr("id");
+
+  // Add the selected element to the userClickedPattern array
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
   checkAnswer(userClickedPattern.length - 1);
 });
 
+// Function that verifies the values selected by the user with the game sequence
 function checkAnswer(currentLevel) {
-  console.log(gamePattern[currentLevel]);
-  console.log(userClickedPattern[currentLevel]);
-  if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+  // If the element the user selects is equal with the element at the same position from the gamePattern array
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     console.log("success");
 
-    if (userClickedPattern.length == gamePattern.length) {
+    // If the 2 arrays have the same length --> a move has been made
+    if (userClickedPattern.length === gamePattern.length) {
+      // After 1s, run the nextSequence() again
       setTimeout(function () {
         nextSequence();
       }, 1000);
     }
   } else {
+    // Play the wrong.mp3 when a user clicks on a wrong button
+    playSound("wrong");
+
+    // Add and remove the .game-over CSS class to the body
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    // Change the h1 text to the following message
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
     console.log("wrong");
   }
 }
@@ -79,9 +95,7 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
 
   var randomColourId = selectColourID(randomChosenColour);
-
   flashAnimation(randomColourId);
-
   playSound(randomChosenColour);
 }
 
